47e236d8a40ad1a392d7d800a1de8c7a93dd5430
MiharaYuta <yuutayuuta3838@gmail.com>
2026-04-15 00:45:53 +0900

    Add files via upload

--- level_editor.py
+++ level_editor.py
@@ -1,23 +1,47 @@
 import bpy
 
 bl_info = {
-"name":"レベルエディタ",
-"author":"Taro Kamata",
-"version":"(1,0)",
-"blender":(3,3,1),
-"location":"",
-"description":"レベルエディタ",
-"warning":"",
-"wiki_url":"",
-"tracker_url":"",
-"category":"Object"
+    "name": "レベルエディタ",
+    "author": "Taro Kamata",
+    "version": (1, 0),
+    "blender": (3, 3, 1),
+    "description": "レベルエディタ",
+    "category": "Object"
 }
 
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    bl_idname = "TOPBAR_MT_my_menu"
+    bl_label = "MyMenu"
+    bl_description = "拡張メニュー by " + bl_info["author"]
+
+    def draw(self, context):
+        self.layout.label(text="メニュー", icon='DOT')
+        self.layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+
+    def submenu(self, context):
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+# Blenderに登録するクラスリスト
+classes = (
+    TOPBAR_MT_my_menu,
+)
+
 def register():
+    # Blenderにクラス登録
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが有効化されました")
-    
+
+
 def unregister():
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+
+    # Blenderからクラスを削除
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
     print("レベルエディタが無効化されました")
-    
+
 if __name__ == "__main__":
     register()
